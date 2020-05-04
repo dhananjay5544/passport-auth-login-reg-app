82ad4eea5c197fa145757433b21d417a9ef7c2b9
Dhananjay Shinde <46316616+dhananjay5544@users.noreply.github.com>
2020-05-05 00:07:34 +0530

    Registration post request handling

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,31 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
+//Handle Register post req
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let err = [];
+
+  //Check Required fields
+  if (!name || !email || !password || !password2) {
+    err.push({ msg: "Please fill in all the fields" });
+  }
+
+  //Check Password match
+  if (password !== password2) {
+    err.push({ msg: "Password doesn't match" });
+  }
+
+  //Check password Length
+  if (password.lenth < 6) {
+    err.push({ msg: "Password should be atleast of 6 characters" });
+  }
+
+  if (err.length > 0) {
+    res.render("register", { err, name, email, password, password2 });
+  } else {
+    res.send("pass");
+  }
+});
+
 module.exports = router;
