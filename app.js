23b55d7774dcde703035894dee474ee7d497f37b
Dhananjay Shinde <46316616+dhananjay5544@users.noreply.github.com>
2020-05-04 23:36:28 +0530

    mongo DB config

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-
+const mongoose = require("mongoose");
 const app = express();
+
+//DB config
+const db = require("./config/keys").MONGO_URL;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
 
 //EJS
 app.use(expressLayouts);
